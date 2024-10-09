22ca0bf7807e0f86bcc89a5c16b7a70c36f85ee9
daniel-takapente <dtakapente@gmail.com>
2024-10-09 23:46:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,23 +19,28 @@ def predict(input_data):
 
 # Create the Streamlit user interface
 st.title("Bank Marketing Prediction App")
+col1, col2 = st.columns(2)
 
+with col1:
 # Creating input fields for new parameters
-age = st.slider('Age', min_value=18, max_value=100, value=30)
-job = st.selectbox('Job', ['admin.', 'blue-collar', 'entrepreneur', 'housemaid', 'management', 
-                             'retired', 'student', 'technician', 'unemployed', 'unknown'])
-marital = st.selectbox('Marital Status', ['divorced', 'married', 'single', 'unknown'])
-education = st.selectbox('Education Level', ['primary', 'secondary', 'tertiary', 'unknown'])
-default = st.selectbox('Credit in Default?', ['yes', 'no'])
-housing = st.selectbox('Housing Loan?', ['yes', 'no'])
-loan = st.selectbox('Personal Loan?', ['yes', 'no'])
-contact = st.selectbox('Contact Communication Type', ['cellular', 'telephone', 'unknown'])
-month = st.selectbox('Last Contact Month of Year', ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 
-                                                    'jul', 'aug', 'sep', 'oct', 'nov', 'dec'])
-day_of_week = st.selectbox('Last Contact Day of Week', ['mon', 'tue', 'wed', 'thu', 'fri'])
-duration = st.number_input('Duration of Last Contact (seconds)', min_value=0, value=0)
-poutcome = st.selectbox('Outcome of the Previous Marketing Campaign', 
-                         ['failure', 'nonexistent', 'success'])
+	age = st.slider('Umuer', min_value=18, max_value=100, value=30)
+	job = st.selectbox('Pekerjaan', ['admin.', 'blue-collar', 'entrepreneur', 'housemaid', 'management', 
+	                             'retired', 'student', 'technician', 'unemployed', 'unknown'])
+	marital = st.selectbox('Status Pernikahan', ['divorced', 'married', 'single', 'unknown'])
+	education = st.selectbox('Tingkat Pendidikan', ['primary', 'secondary', 'tertiary', 'unknown'])
+	default = st.selectbox('Memiliki Kredit ?', ['yes', 'no'])
+	housing = st.selectbox('Memiliki KPR?', ['yes', 'no'])
+
+with col2:
+	loan = st.selectbox('Memiliki Personal Loan?', ['yes', 'no'])
+	contact = st.selectbox('Alat Komunikasi ', ['cellular', 'telephone', 'unknown'])
+	month = st.selectbox('Bulan Terakhid Di hubungi', ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 
+	                                                    'jul', 'aug', 'sep', 'oct', 'nov', 'dec'])
+	day_of_week = st.selectbox('Hari terakhir di hubungi', ['mon', 'tue', 'wed', 'thu', 'fri'])
+	duration = st.number_input('Durasi percakapan terakhir ', min_value=0, value=0)
+	poutcome = st.selectbox('Hasil Kampanye marketing sebelumnya', 
+	                         ['failure', 'nonexistent', 'success'])
+
 
 # Collect input data into a list
 input_data = [age, job, marital, education, default, housing, loan, contact, month, 
